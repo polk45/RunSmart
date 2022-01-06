0c63eb40444f79e424344eae4fd2d8880cb8ec10
Artem <example@gmail.com>
2022-01-06 17:23:28 +0300

    Add new section -catalog

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,17 @@
-/* for (let i = 1; i < 8; i++) {
-    console.log(i); 
-} */
-
-function logging(a, b) {
-    console.log ( a + b )
-}
-
-logging(3 , 5);
-logging(10 , 5);
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        /* adaptiveHeight: true, */
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/prevarrow.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/nextarrow.png"></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                dots: true,
+                arrows: false
+              }
+            },
+          ]
+      });
+  });
