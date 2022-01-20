8d72273c4c40bec301d469d1afaddd1467968264
Artem <example@gmail.com>
2022-01-20 16:03:48 +0300

    add mail and buton to up

--- src/js/script.js
+++ src/js/script.js
@@ -77,4 +77,38 @@ $(document).ready(function(){
     valideForms('#order form');
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function (e) {
+      e.preventDefault();
+      if (!$(this).valid()) {
+        return;
+      }
+      $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+      }).done(function () {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+
+        $('form').trigger('reset');
+      });
+      return false;
+    });
+
+    //smooth scrool and pageup
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 1000) {
+        $('.pageup').fadeIn();
+      } else {
+        $('.pageup').fadeOut();
+      }
+    });
+    $("a[href^='#']").click(function(){
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+  });
+
   });
